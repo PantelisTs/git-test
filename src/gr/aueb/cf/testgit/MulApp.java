a76a7689a87bfd8a2be8efa8f05a6b6f013b5002
PantelisTs <pantsotsgr@gmail.com>
2025-12-02 19:50:21 +0200

    feat: add mulapp

---
+++ src/gr/aueb/cf/testgit/MulApp.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.testgit;
+
+public class MulApp {
+    static void main() {
+        int a = 4;
+        int b = 2;
+        System.out.println(Mul(a, b));
+    }
+
+    public static int Mul(int a, int b) {
+        return a / b;
+    }
+}
