e5ddb1155046d206f23644c3f808c266254f5cac
PantelisTs <pantsotsgr@gmail.com>
2025-12-02 19:23:45 +0200

    feat: add addapp

---
+++ src/gr/aueb/cf/testgit/AddApp.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.testgit;
+
+public class AddApp {
+
+    static void main() {
+        int a = 1;
+        int b = 2;
+
+        System.out.println(add(a, b));
+
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
